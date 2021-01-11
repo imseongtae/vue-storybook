f630be35ab0674621f177473311e9119d3810b8a
imseongtae <dev.haemil@gmail.com>
2021-01-12 02:32:21 +0900

    feat: Add Typo component

---
+++ src/components/typography/Typo.stories.js
@@ -0,0 +1,21 @@
+import Typo from './Typo.vue';
+
+export default {
+	title: 'Example/Typo',
+	component: Typo,
+};
+
+const Template = (args, { argTypes }) => ({
+	props: Object.keys(argTypes),
+	components: { Typo },
+	template: `<typo v-bind="$props" />`,
+});
+
+export const typoStyle = Template.bind({});
+typoStyle.args = {
+	title: 'This text is title typo, 우리 아이가 혼자서 집중하는',
+	bodyText:
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. 우리 아이가 혼자서 집중하는',
+	subTitle: 'This text is sub title typo 우리 아이가 혼자서 집중하는',
+	subBodyText: 'This text is sub body text typo 우리 아이가 혼자서 집중하는',
+};
---
+++ src/components/typography/Typo.vue
@@ -0,0 +1,113 @@
+<template>
+	<div class="container">
+		<h2 v-if="title" class="typo title">{{ title }}</h2>
+		<p v-if="bodyText" class="typo body-text">
+			{{ bodyText }}
+		</p>
+		<h3 v-if="subTitle" class="typo sub-title">{{ subTitle }}</h3>
+		<p v-if="subBodyText" class="typo sub-body-text">{{ subBodyText }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Typo',
+	props: {
+		title: {
+			type: String,
+			required: true,
+		},
+		bodyText: {
+			type: String,
+			required: true,
+		},
+		subTitle: {
+			type: String,
+			required: true,
+		},
+		subBodyText: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+	* {
+		margin-bottom: 1rem;
+	}
+}
+.typo {
+	color: #000;
+	&.title {
+		font: {
+			size: 24px;
+			weight: 700;
+			style: normal;
+		}
+		letter-spacing: -0.05rem;
+	}
+	&.body-text {
+		font: {
+			// 폰트 사이즈 16px로 조정하기
+			size: 16px;
+			weight: 400;
+		}
+		letter-spacing: -0.05rem;
+	}
+	&.sub-title {
+		font: {
+			size: 16px; // 16px
+			weight: 700;
+		}
+		letter-spacing: -0.01rem;
+	}
+	&.sub-body-text {
+		font: {
+			size: 14px; // 14px
+			weight: 400;
+		}
+		letter-spacing: -0.01rem;
+	}
+
+	&.reference {
+		font: {
+			size: 14px; // 14px
+			weight: 400;
+		}
+		letter-spacing: -0.01rem;
+	}
+
+	&.detail-section-title {
+		font: {
+			size: 17px;
+			weight: 700;
+		}
+		letter-spacing: -0.01rem;
+	}
+	&.detail-section-text {
+		font: {
+			size: 14px;
+			weight: 400;
+		}
+		color: #666;
+		letter-spacing: -0.01rem;
+	}
+
+	// Font weight 및 style(normal)
+	&.thin {
+		font-weight: 300;
+	}
+	&.normal {
+		font-weight: 400;
+	}
+	&.bold {
+		font-weight: 700;
+	}
+	&.style-normal {
+		font-style: normal;
+	}
+}
+</style>
