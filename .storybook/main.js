3060c6e2924e846b9cece4fe8f2ab52154baafef
imseongtae <dev.haemil@gmail.com>
2021-01-18 05:44:50 +0900

    chore: Update storybook config setting

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    // "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.mdx",    
     "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
