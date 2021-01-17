3e2bcf39348c09b8af9fd23f291f1bb4705e604c
imseongtae <dev.haemil@gmail.com>
2021-01-18 05:22:46 +0900

    chore: Project setting

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Components
+import AppButton from './components/Button/AppButton.vue';
+import AppInput from './components/Input/AppInput.vue';
+
+const components = [AppButton, AppInput];
+
+const install = Vue => {
+	components.forEach(component => {
+		Vue.component(component.name, component);
+	});
+};
+
+// auto install if used in browser
+if (typeof window !== 'undefined' && window.Vue) {
+	install(window.Vue);
+}
+
+export { AppButton, AppInput };
+
+export default install;
