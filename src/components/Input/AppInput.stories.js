e4f93bb505d6acf2d0822673814cc0efb32359f7
imseongtae <dev.haemil@gmail.com>
2021-01-18 05:23:41 +0900

    refactor: Update AppInput component

--- src/components/Input/AppInput.stories.js
+++ src/components/Input/AppInput.stories.js
@@ -3,10 +3,15 @@ import AppInput from './AppInput.vue';
 export default {
 	title: 'Example/Input',
 	component: AppInput,
+	argTypes: {
+		size: {
+			control: { type: 'select', options: ['small', 'medium', 'large'] },
+		},
+	},
 	decorators: [
 		() => ({
 			template: `				
-			<div style="margin: 3em; max-width: 1024px"><h3>WIP</h3><br /> <story/></div>`,
+			<div style="margin: 3em; max-width: 1024px"><story/></div>`,
 		}),
 	],
 };
@@ -14,13 +19,91 @@ export default {
 const Template = (args, { argTypes }) => ({
 	props: Object.keys(argTypes),
 	components: { AppInput },
-	template: `<app-input v-bind="$props" />`,
+	template: `<app-input v-model="data" v-bind="$props" @onInput="onInput"  />`,
+	data() {
+		return {
+			data: '',
+		};
+	},
 });
 
-export const appInput = Template.bind({});
-appInput.args = {
-	id: '1',
-	type: 'text',
-	vModelData: '',
-	placeholder: '전화번호를 입력해주세요',
+// TODO
+// - CSS or 아이콘으로 체크 표현하기, 아이콘에 적당한 친구가 없음
+
+export const normal = Template.bind({});
+normal.args = {
+	id: 'normal',
+	state: 'normal',
+	placeholder: 'normal input',
+	disabled: false,
 };
+
+export const valid = Template.bind({});
+valid.args = {
+	id: 'valid',
+	state: 'valid',
+	placeholder: 'valid input',
+};
+
+export const invalid = Template.bind({});
+invalid.args = {
+	id: 'invalid',
+	state: 'invalid',
+	placeholder: 'invalid input',
+};
+
+export const focus = Template.bind({});
+focus.args = {
+	id: 'focus',
+	state: 'focus',
+	placeholder: 'focus input',
+};
+
+export const readonly = Template.bind({});
+readonly.args = {
+	id: 'readonly',
+	state: 'readonly',
+	placeholder: 'readonly input',
+	readonly: true,
+};
+
+export const disabled = Template.bind({});
+disabled.args = {
+	id: 'disabled',
+	state: 'disabled',
+	placeholder: 'disabled input',
+	disabled: true,
+};
+
+export const sizes = (args, { argTypes }) => ({
+	components: { AppInput },
+	props: Object.keys(argTypes),
+	template: `
+		<ul class="list">		
+			<li class="list-item">
+				<p class="label">Small</p>
+				<app-input :id="'small'" :size="'small'" :placeholder="'Small'" @onInput="onInput" />
+			</li>
+			<li class="list-item">
+				<p class="label">Medium</p>
+				<app-input :id="'medium'" :size="'medium'" :placeholder="'Medium'" @onInput="onInput" />
+			</li>
+			<li class="list-item">
+				<p class="label">Large</p>
+				<app-input :id="'large'" :size="'large'" :placeholder="'Large'" @onInput="onInput" />
+			</li>
+		</ul>
+	`,
+});
+
+// export const phoneNumberInput = Template.bind({});
+// phoneNumberInput.args = {
+// 	id: 'phoneNumber',
+// 	placeholder: '전화번호를 입력해주세요',
+// };
+
+// function validatePhoneNumber(phone) {
+// 	const ph1 = /^\+82\s1([0|1|6|7|8|9]?)-?([0-9]{4})-?([0-9]{4})$/;
+// 	const ph2 = /^01([0|1|6|7|8|9]?)-?([0-9]{4})-?([0-9]{4})$/;
+// 	return ph1.test(phone) || ph2.test(phone);
+// }
--- src/components/Input/AppInput.vue
+++ src/components/Input/AppInput.vue
@@ -1,11 +1,14 @@
 <template>
 	<input
 		:id="id"
-		:v-model="vModelData"
-		class="app-input"
 		:type="type"
+		v-model="data"
 		:placeholder="placeholder"
 		aria-required="true"
+		:class="classes"
+		@input="onInput"
+		:readonly="readonly"
+		:disabled="disabled"
 	/>
 </template>
 
@@ -13,35 +16,137 @@
 export default {
 	name: 'AppInput',
 	props: {
+		// label 요소 속성 for에 대응하기 위한 id
 		id: {
 			type: String,
 			required: true,
 		},
 		type: {
 			type: String,
-			required: true,
-		},
-		vModelData: {
-			type: String,
-			required: true,
+			required: false,
+			default: 'text',
 		},
 		placeholder: {
 			type: String,
 			required: true,
 		},
+		valid: {
+			type: Boolean,
+			required: false,
+			default: false,
+		},
+		invalid: {
+			type: Boolean,
+			required: false,
+			default: false,
+		},
+		focus: {
+			type: Boolean,
+			required: false,
+			default: false,
+		},
+		// readonly와 disabled 속성은 form submit시 데이터 전송 차이가 발생함
+		readonly: {
+			type: Boolean,
+			required: false,
+			default: false,
+		},
+		disabled: {
+			type: Boolean,
+			required: false,
+			default: false,
+		},
+		state: {
+			type: String,
+			default: 'normal',
+			validator(value) {
+				return (
+					[
+						'normal',
+						'focus',
+						'valid',
+						'invalid',
+						'readonly',
+						'disabled',
+					].indexOf(value) !== -1
+				);
+			},
+		},
+		// TODO: 사이즈는 수치로도 받을 수 있도록
+		size: {
+			type: String,
+			default: 'medium',
+			validator(value) {
+				return ['small', 'medium', 'large'].indexOf(value) !== -1;
+			},
+		},
 	},
-	methods: {},
+	data() {
+		return {
+			data: '',
+		};
+	},
+	computed: {
+		classes() {
+			return {
+				'app-input': true,
+				valid: this.valid,
+				invalid: this.invalid,
+				[`app-input--${this.state}`]: true,
+				[`app-input--${this.size}`]: true,
+			};
+		},
+	},
+	methods: {
+		onInput() {
+			this.$emit('onInput', this.data);
+		},
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 .app-input {
 	font-size: 16px;
-	margin-bottom: 1rem;
 	padding-left: 1rem;
 	height: 3rem;
 	line-height: 3rem;
-	border: 1px solid #eaeaea;
+	// border: 1px solid #eaeaea;
 	border-radius: 4px;
+	outline: none;
+	// 각 input의 width 값은 변경될 수 있음
+	&--small {
+		font-size: 14px;
+		padding: 10px 16px;
+		width: 288px;
+	}
+	&--medium {
+		padding: 11px 16px;
+		font-size: 16px;
+		width: 320px;
+	}
+	&--large {
+		font-size: 16px;
+		padding: 15px 20px;
+		width: 392px;
+	}
+
+	// input state style
+	&--normal {
+		border: 2px solid #eaeaea;
+	}
+	&:focus {
+		border: 2px solid #4f00ff;
+	}
+	&--focus {
+		border: 2px solid #4f00ff;
+	}
+	// !important를 설정하지 않으면 focus가 해제되어야 border style이 적용된다.
+	&--valid {
+		border: 2px solid #27ae60 !important;
+	}
+	&--invalid {
+		border: 2px solid #eb5757 !important;
+	}
 }
 </style>
